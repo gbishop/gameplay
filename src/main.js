102b58c5eaa0ca1d6e332d8ee3ae7043b9fd8f80
Gary Bishop <gary.bishop.unc@gmail.com>
2024-10-28 09:35:01 -0400

    Index is switch accessible.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { decode } from "./url";
 import { YouTubePlayer } from "./yt-player";
 // @ts-ignore
 import links from "./links.json";
+const movers = new Set([" ", "ArrowRight"]);
+const choosers = new Set(["Enter", "ArrowLeft"]);
 
 /** @param {Game} game */
 function play(game) {
@@ -61,57 +63,56 @@ function play(game) {
     message.removeAttribute("hide");
   }
 
-  const movers = new Set([" ", "ArrowRight"]);
-  const choosers = new Set(["Enter", "ArrowLeft"]);
   document.addEventListener("keyup", (event) => {
+    console.log(event);
     if (movers.has(event.key)) {
-      const buttons = [...message.querySelectorAll("button")];
-      const selected = /** @type {HTMLButtonElement} */ (
-        message.querySelector("button[selected]")
-      );
+      const targets = [...document.querySelectorAll("button,a")];
+      const selected = document.querySelector("[selected]");
       let index = 0;
       if (selected) {
-        index = (buttons.indexOf(selected) + 1) % buttons.length;
+        index = (targets.indexOf(selected) + 1) % targets.length;
         selected.removeAttribute("selected");
       }
-      buttons[index].setAttribute("selected", "");
+      targets[index].setAttribute("selected", "");
     } else if (choosers.has(event.key)) {
-      const selected = /** @type {HTMLButtonElement} */ (
-        message.querySelector("button[selected]")
-      );
-      if (selected) {
+      const selected = document.querySelector("[selected]");
+      if (selected instanceof HTMLElement) {
         choose(selected);
       }
     }
   });
 
-  message.addEventListener("pointerup", (event) => {
+  document.addEventListener("pointerup", (event) => {
     if (event.target instanceof HTMLButtonElement) {
       choose(event.target);
     }
   });
 
-  /** @param {HTMLButtonElement} button */
-  function choose(button) {
-    const next = parseInt(button.getAttribute("next") || "0");
-    if (next == 0) {
-      TPIndex += 1;
-      go();
-    } else if (next == -1) {
-      TPIndex = 0;
-      player.seek(game.start);
-      go();
-    } else if (next == -2) {
-      console.log("quit");
-    } else {
-      player.seek(next);
-      for (let i = 0; i < game.timePoints.length; i++) {
-        if (game.timePoints[i].time >= next) {
-          TPIndex = i;
-          break;
+  /** @param {HTMLElement} target */
+  function choose(target) {
+    if (target instanceof HTMLButtonElement) {
+      const next = parseInt(target.getAttribute("next") || "0");
+      if (next == 0) {
+        TPIndex += 1;
+        go();
+      } else if (next == -1) {
+        TPIndex = 0;
+        player.seek(game.start);
+        go();
+      } else if (next == -2) {
+        console.log("quit");
+      } else {
+        player.seek(next);
+        for (let i = 0; i < game.timePoints.length; i++) {
+          if (game.timePoints[i].time >= next) {
+            TPIndex = i;
+            break;
+          }
         }
+        go();
       }
-      go();
+    } else if (target instanceof HTMLAnchorElement) {
+      target.click();
     }
   }
 
@@ -122,11 +123,7 @@ function play(game) {
   }
 }
 
-if (location.search) {
-  const game = decode(new URL(location.href));
-  console.log({ game });
-  play(game);
-} else {
+function index() {
   const html = [];
   for (const link of links) {
     const url = new URL(location.href);
@@ -135,9 +132,9 @@ if (location.search) {
     const videoId = sp.get("v");
     const src = `https://img.youtube.com/vi/${videoId}/mqdefault.jpg`;
     const cell = `
-      <a href="${url}">
+      <a href="${link}">
         <figure>
-          <img src="${src}"\>
+          <img src="${src}" loading="lazy"\>
           <figcaption>${sp.get("t")}</figcaption>
         </figure>
       </a>`;
@@ -153,4 +150,67 @@ if (location.search) {
   }
   app.innerHTML = html.join("");
   message.style.display = "none";
+  document.addEventListener("keyup", (event) => {
+    console.log(event);
+    if (movers.has(event.key)) {
+      const targets = [...document.querySelectorAll("a")];
+      const selected = document.activeElement;
+      let index = 0;
+      if (selected instanceof HTMLAnchorElement) {
+        index = (targets.indexOf(selected) + 1) % targets.length;
+      }
+      targets[index].focus();
+    } else if (choosers.has(event.key)) {
+      const selected = /** @type {HTMLAnchorElement} */ (
+        document.querySelector("a[selected]")
+      );
+      if (selected instanceof HTMLAnchorElement) {
+        selected.click();
+      }
+    }
+  });
+  const ReducedMotion = window.matchMedia(
+    `(prefers-reduced-motion: reduce)`,
+  ).matches;
+  let timer;
+  if (!ReducedMotion) {
+    app.addEventListener("focusin", (event) => {
+      if (event.target instanceof HTMLAnchorElement) {
+        const figure = event.target.firstElementChild;
+        if (figure instanceof HTMLElement) {
+          // center of window
+          const wc = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+          // figure bounding box
+          const fcr = figure.getBoundingClientRect();
+          // center of figure
+          const fc = { x: fcr.x + fcr.width / 2, y: fcr.y + fcr.height / 2 };
+          // translate center of figure to center of screen
+          const delta = { x: wc.x - fc.x, y: wc.y - fc.y };
+          figure.style.transform = `translate(${delta.x}px, ${delta.y}px) scale(2)`;
+          figure.style.transition = `transform 0.2s linear`;
+          timer = setInterval(() => (figure.style.transform = ""), 5000);
+        }
+      }
+    });
+    app.addEventListener("focusout", (event) => {
+      if (event.target instanceof HTMLAnchorElement) {
+        const figure = event.target.firstElementChild;
+        if (figure instanceof HTMLElement) {
+          figure.style.transform = "";
+          if (timer) {
+            clearInterval(timer);
+            timer = 0;
+          }
+        }
+      }
+    });
+  }
+}
+
+if (location.search) {
+  const game = decode(new URL(location.href));
+  console.log({ game });
+  play(game);
+} else {
+  index();
 }
